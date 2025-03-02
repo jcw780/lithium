ca3e2244754a19dafa851492aa6625c55bca20f5
2No2Name <2No2Name@web.de>
2025-03-02 18:30:10 +0100

    Fix transfer API compat introducing transfer api dependency

--- fabric/src/main/java/net/caffeinemc/mods/lithium/fabric/FabricModCompat.java
+++ fabric/src/main/java/net/caffeinemc/mods/lithium/fabric/FabricModCompat.java
@@ -2,6 +2,7 @@ package net.caffeinemc.mods.lithium.fabric;
 
 import net.caffeinemc.mods.lithium.common.services.PlatformModCompat;
 import net.fabricmc.fabric.api.transfer.v1.item.ItemStorage;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.HopperBlock;
@@ -10,8 +11,17 @@ import net.minecraft.world.level.block.state.BlockState;
 
 public class FabricModCompat implements PlatformModCompat {
 
+    private static final boolean HAS_TRANSFER_API = FabricLoader.getInstance().isModLoaded("fabric-transfer-api-v1");
+
     @Override
     public boolean canHopperInteractWithApiBlockInventory(HopperBlockEntity hopperBlockEntity, BlockState hopperState, boolean extracting) {
+        if (!HAS_TRANSFER_API) {
+            return false;
+        }
+        return canFindApiInventory(hopperBlockEntity, hopperState, extracting);
+    }
+
+    private static boolean canFindApiInventory(HopperBlockEntity hopperBlockEntity, BlockState hopperState, boolean extracting) {
         Direction direction = extracting ? Direction.UP : hopperState.getValue(HopperBlock.FACING);
         BlockPos targetPos = hopperBlockEntity.getBlockPos().relative(direction);
 
