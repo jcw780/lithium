f14b69a04c30511c8ef0a8fa8344712038153b41
2No2Name <2No2Name@web.de>
2025-04-06 18:57:32 +0200

    Bump Mod Version

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ val FABRIC_API_VERSION by extra { "0.119.5+1.21.5" }
 val PARCHMENT_VERSION by extra { null }
 
 // https://semver.org/
-val MOD_VERSION by extra { "0.16.0" }
+val MOD_VERSION by extra { "0.16.1" }
 
 allprojects {
     apply(plugin = "java")
