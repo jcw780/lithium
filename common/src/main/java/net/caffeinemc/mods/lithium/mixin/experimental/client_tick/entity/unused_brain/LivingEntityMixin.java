693c43017b25acd408bf0faaf1b1af18e6107ee6
2No2Name <2No2Name@web.de>
2025-04-08 20:32:50 +0200

    Fix mod compatibility issues with null brain
    https://github.com/CaffeineMC/lithium/issues/641

--- common/src/main/java/net/caffeinemc/mods/lithium/mixin/experimental/client_tick/entity/unused_brain/LivingEntityMixin.java
+++ common/src/main/java/net/caffeinemc/mods/lithium/mixin/experimental/client_tick/entity/unused_brain/LivingEntityMixin.java
@@ -1,5 +1,6 @@
 package net.caffeinemc.mods.lithium.mixin.experimental.client_tick.entity.unused_brain;
 
+import com.google.common.collect.ImmutableList;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.mojang.serialization.Dynamic;
@@ -8,14 +9,20 @@ import net.minecraft.world.entity.ai.Brain;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
+import java.util.List;
+
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin {
+    private static final Brain<?> DUMMY_BRAIN = new Brain<>(List.of(), List.of(), ImmutableList.of(), () -> {
+        throw new IllegalStateException("Trying to serialize client side brain! If you really want this, disable lithium's client side brain optimization!");
+    });
+
     @WrapOperation(
             method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;makeBrain(Lcom/mojang/serialization/Dynamic;)Lnet/minecraft/world/entity/ai/Brain;")
     )
     private Brain<?> doNotCreateClientBrain(LivingEntity instance, Dynamic<?> dynamic, Operation<Brain<?>> original) {
         if (instance.level().isClientSide()) {
-            return null;
+            return DUMMY_BRAIN;
         }
         return original.call(instance, dynamic);
     }
