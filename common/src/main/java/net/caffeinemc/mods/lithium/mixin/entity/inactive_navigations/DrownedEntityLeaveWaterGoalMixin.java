dae90df35591525b8824ed37c57fcdfe191e4ee4
2No2Name <2No2Name@web.de>
2025-12-09 21:39:24 +0100

    Fix Drowned injection point

--- common/src/main/java/net/caffeinemc/mods/lithium/mixin/entity/inactive_navigations/DrownedEntityLeaveWaterGoalMixin.java
+++ common/src/main/java/net/caffeinemc/mods/lithium/mixin/entity/inactive_navigations/DrownedEntityLeaveWaterGoalMixin.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(targets = "net/minecraft/world/entity/monster/Drowned$DrownedGoToBeachGoal")
+@Mixin(targets = "net/minecraft/world/entity/monster/zombie/Drowned$DrownedGoToBeachGoal")
 public class DrownedEntityLeaveWaterGoalMixin {
     @Shadow
     @Final
