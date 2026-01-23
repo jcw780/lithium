930fa8f5255f885185e58648ef48fbcebc717835
jcw780 <30562395+jcw780@users.noreply.github.com>
2026-01-23 18:14:05 -0500

    simplify sculk sensor ticker mixin

--- common/src/main/java/net/caffeinemc/mods/lithium/mixin/world/block_entity_ticking/sleeping/sculk_sensor/CalibratedSculkSensorBlockMixin.java
+++ common/src/main/java/net/caffeinemc/mods/lithium/mixin/world/block_entity_ticking/sleeping/sculk_sensor/CalibratedSculkSensorBlockMixin.java
@@ -1,18 +1,17 @@
 package net.caffeinemc.mods.lithium.mixin.world.block_entity_ticking.sleeping.sculk_sensor;
 
 import net.caffeinemc.mods.lithium.common.block.entity.SleepingBlockEntity;
-import net.caffeinemc.mods.lithium.common.block.entity.sleeping_sculk_sensor.CustomVibrationTicker;
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.CalibratedSculkSensorBlock;
-import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.entity.BlockEntityTicker;
-import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.entity.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
-import org.jspecify.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(CalibratedSculkSensorBlock.class)
 public abstract class CalibratedSculkSensorBlockMixin extends BaseEntityBlock {
@@ -20,23 +19,10 @@ public abstract class CalibratedSculkSensorBlockMixin extends BaseEntityBlock {
         super(properties);
     }
 
-    /**
-     * @author
-     * @reason
-     */
-    @Nullable
-    @Overwrite
-    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState blockState, BlockEntityType<T> blockEntityType) {
-        return !level.isClientSide()
-                ? createTickerHelper(
-                blockEntityType,
-                BlockEntityType.CALIBRATED_SCULK_SENSOR,
-                (levelx, blockPos, blockStatex, calibratedSculkSensorBlockEntity) ->
-                        CustomVibrationTicker.lithium$sleepingTicker(levelx, calibratedSculkSensorBlockEntity.getVibrationData(),
-                                calibratedSculkSensorBlockEntity.getVibrationUser(),
-                                ((SleepingBlockEntity) calibratedSculkSensorBlockEntity)::lithium$startSleeping
-                        )
-        )
-                : null;
+    @Inject(method = {"method_49813"}, at = @At(value = "RETURN"))
+    private static void checkSleep(Level level, BlockPos blockPos, BlockState blockState, CalibratedSculkSensorBlockEntity calibratedSculkSensorBlockEntity, CallbackInfo ci) {
+        if (calibratedSculkSensorBlockEntity.getVibrationData().getCurrentVibration() == null) {
+            ((SleepingBlockEntity) calibratedSculkSensorBlockEntity).lithium$startSleeping();
+        }
     }
 }
