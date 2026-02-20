1b175c0ce3a0da70410e4eebefbb34c257912df3
jcw780 <30562395+jcw780@users.noreply.github.com>
2026-02-20 00:42:42 -0500

    fix faulty behavior removal logic

--- common/src/main/java/net/caffeinemc/mods/lithium/mixin/ai/useless_behaviors/BrainMixin.java
+++ common/src/main/java/net/caffeinemc/mods/lithium/mixin/ai/useless_behaviors/BrainMixin.java
@@ -1,34 +1,61 @@
 package net.caffeinemc.mods.lithium.mixin.ai.useless_behaviors;
 
 import com.google.common.collect.ImmutableList;
-import com.llamalad7.mixinextras.sugar.Local;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 import com.mojang.datafixers.util.Pair;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.Brain;
 import net.minecraft.world.entity.ai.behavior.BehaviorControl;
 import net.minecraft.world.entity.ai.memory.MemoryModuleType;
 import net.minecraft.world.entity.ai.memory.MemoryStatus;
 import net.minecraft.world.entity.schedule.Activity;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 
+import java.util.Map;
 import java.util.Set;
 
-/**
- * This is so one can just null out behaviors earlier to prevent them from being added - otherwise the game will crash.
- * Note: Could also use a sentinel instead of null if preferred.
- *
- * @author jcw780
- */
 @Mixin(Brain.class)
-public class BrainMixin {
-    @Inject(method = "addActivityAndRemoveMemoriesWhenStopped", cancellable = true, at = @At(value = "INVOKE", target = "Ljava/util/Map;computeIfAbsent(Ljava/lang/Object;Ljava/util/function/Function;)Ljava/lang/Object;"))
-    private void removeNullBehaviors(Activity activity, ImmutableList<? extends Pair<Integer, ? extends BehaviorControl<?>>> immutableList,
-                                  Set<Pair<MemoryModuleType<?>, MemoryStatus>> set, Set<MemoryModuleType<?>> set2,
-                                  CallbackInfo ci, @Local Pair<Integer, ? extends BehaviorControl<?>> pair) {
-        if (pair.getSecond() == null) {
-            ci.cancel();
+public abstract class BrainMixin<E extends LivingEntity> {
+    @Shadow
+    @Final
+    private Map<Integer, Map<Activity, Set<BehaviorControl<? super E>>>> availableBehaviorsByPriority;
+
+    @Shadow
+    @Final
+    private Map<Activity, Set<Pair<MemoryModuleType<?>, MemoryStatus>>> activityRequirements;
+
+    @Shadow
+    @Final
+    private Map<Activity, Set<MemoryModuleType<?>>> activityMemoriesToEraseWhenStopped;
+
+    /**
+     * @author jcw780
+     * @reason This is so one can just null out behaviors earlier to prevent them from being added - otherwise the game will crash.
+     * Note: Could also use a sentinel instead of null if preferred.
+     */
+    @Overwrite
+    public void addActivityAndRemoveMemoriesWhenStopped(
+            Activity activity,
+            ImmutableList<? extends Pair<Integer, ? extends BehaviorControl<? super E>>> immutableList,
+            Set<Pair<MemoryModuleType<?>, MemoryStatus>> set,
+            Set<MemoryModuleType<?>> set2
+    ) {
+        this.activityRequirements.put(activity, set);
+        if (!set2.isEmpty()) {
+            this.activityMemoriesToEraseWhenStopped.put(activity, set2);
+        }
+
+        for (Pair<Integer, ? extends BehaviorControl<? super E>> pair : immutableList) {
+            if (pair.getSecond() == null) {
+                continue;
+            }
+            ((Set)((Map)this.availableBehaviorsByPriority.computeIfAbsent(pair.getFirst(), integer -> Maps.newHashMap()))
+                    .computeIfAbsent(activity, activityx -> Sets.newLinkedHashSet()))
+                    .add(pair.getSecond());
         }
     }
 }
