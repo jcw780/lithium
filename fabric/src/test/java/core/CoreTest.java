9b5f3efd8a0c8c3cf6fc904515cb58acd827b469
2No2Name <2No2Name@web.de>
2026-03-25 00:33:51 +0100

    Fix core test

--- fabric/src/test/java/core/CoreTest.java
+++ fabric/src/test/java/core/CoreTest.java
@@ -4,7 +4,6 @@ import net.caffeinemc.mods.lithium.common.util.change_tracking.ChangePublisher;
 import net.minecraft.SharedConstants;
 import net.minecraft.server.Bootstrap;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -17,21 +16,8 @@ public class CoreTest {
         Bootstrap.bootStrap();
     }
 
-
-    @Test
-    void testItemStackInitialized() {
-        ItemStack diamondStack = new ItemStack(Items.DIAMOND, 4);
-
-        Assertions.assertTrue(diamondStack.is(Items.DIAMOND));
-        Assertions.assertEquals(4, diamondStack.getCount());
-
-    }
-
     @Test
     void testMixinApply() {
-        ItemStack diamondStack = new ItemStack(Items.DIRT, 4);
-
-        //noinspection ConstantValue,SimplifiableAssertion,DataFlowIssue
-        Assertions.assertTrue(((Object) diamondStack) instanceof ChangePublisher);
+        Assertions.assertTrue(ChangePublisher.class.isAssignableFrom(ItemStack.class));
     }
 }
